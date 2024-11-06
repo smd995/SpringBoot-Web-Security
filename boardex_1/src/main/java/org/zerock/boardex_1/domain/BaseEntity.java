122a0dc4f8fcbf5d9609d375670a89a652e1c5e3
smd995 <112795934+smd995@users.noreply.github.com>
2024-11-06 11:21:47 +0900

    setting

---
+++ boardex_1/src/main/java/org/zerock/boardex_1/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package org.zerock.boardex_1.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regDate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime modDate;
+}
